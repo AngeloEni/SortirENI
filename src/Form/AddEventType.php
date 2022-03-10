8a06e7889df36104440fcfbe93341675cddadc33
Annest WHELDON <annest.wheldon2021@campus-eni.fr>
2022-03-10 16:13:04 +0100

    tous les filtres

---
+++ src/Form/AddEventType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddEventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('dateTimeStart', DateType::class, [
+                'required' => true,
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('registrationClosingDate', DateType::class, [
+                'required' => true,
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('maxParticipants')
+            ->add('duration', NumberType::class, [
+                'min' => 1,
+                'max' => 1000,
+            ])
+            ->add('eventInfo', TextareaType::class,)
+            ->add('venue', TextType::class)
+
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+            ->add('publish', SubmitType::class, ['label' => 'Publier'])
+            ->add('cancel', ResetType::class, ['label' => 'Annuler']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
