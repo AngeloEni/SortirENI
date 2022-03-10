65cca7405c9c71a76357bf9460b178712b3e3f7f
AngeloEni <angelo.fernandes2021@campus-eni.fr>
2022-03-10 23:27:46 +0100

    fin de la page consulter un event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\EventRepository;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Expr\Array_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +16,23 @@ class EventController extends AbstractController
     /**
      * @Route("/event/{id}", name="app_event")
      */
-    public function index(int $id, EventRepository $eventRepository): Response
+    public function index(int $id, EventRepository $eventRepository, EntityManagerInterface $em, ParticipantRepository $pr): Response
     {
         $event = $eventRepository->find($id);
-        $d = $event->getParticipants();
-        dd($d);
+
+        $eventParticipant = $event->getParticipants();
+
+        $arrayParticipant = array();
+
+        foreach ($eventParticipant as $u){
+            array_push($arrayParticipant, $u);
+        }
+
+
+
         return $this->render('event/index.html.twig', [
-            'event' => $event
+            'event' => $event,
+            'participants' => $arrayParticipant
         ]);
     }
 }
