c372da3e19f4df3d778a4781c0ab0fe6b411c481
Angelo <93374183+AngeloEni@users.noreply.github.com>
2022-03-10 14:18:06 +0100

    Ajout de la fonction ajouter une image , cette dernière sera supprimé si une image pour le participant est déjà présente

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -7,21 +7,22 @@ use App\Form\ParticipantType;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ParticipantController extends AbstractController
 {
     /**
      * @Route("/participant", name="app_participant")
      */
-    public function index(ParticipantRepository $pRepo, \Symfony\Component\HttpFoundation\Request $req, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
+    public function index(ParticipantRepository $pRepo, \Symfony\Component\HttpFoundation\Request $req, EntityManagerInterface $em, UserPasswordHasherInterface $hasher, SluggerInterface $slugger): Response
     {
-        //mock test
-        //$participant = $pRepo->find(1);
 
-
+        //récupération de l'utilisateur en session
         $participant = $this->getUser();
         $form = $this->createForm(ParticipantType::class, $participant);
         $form->handleRequest($req);
@@ -36,6 +37,33 @@ class ParticipantController extends AbstractController
                      $participant->setPassword($hashedPassword);
                 }
 
+            $brochureFile = $form->get('brochure')->getData();
+
+
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                //méthode pour supprimer l'ancienne image du participant
+                $this->deleteImage($participant->getImage(),$em);
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('image'),
+                        $newFilename
+                    );
+
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $participant->setImage($newFilename);
+            }
 
             $em->persist($participant);
             $em->flush();
@@ -44,6 +72,26 @@ class ParticipantController extends AbstractController
 
         return $this->render('participant/index.html.twig', [
             'form' => $form->createView(),
+            'particpant' => $participant
         ]);
+    }
+
+
+    public function deleteImage($name)
+    {
+        /**
+         * Je gère la suppression du dossier "uploads" ou l'image est stockée
+         */
+        //Je récupère le nom de l'image
+        $filename = $name;
+
+        // Je crée une instance de kla classe fileSystem
+        $fileSystem = new Filesystem();
+        //Je supprime l'image du dossier
+        $projectDir = $this->getParameter('kernel.project_dir');
+        $fileSystem->remove($projectDir.'/public/uploads/'.$filename);
+
+        return new Response('deleted', Response::HTTP_OK);
+
     }
 }
--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -81,6 +81,11 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $pseudo;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
 
     public function __construct()
     {
@@ -304,6 +309,18 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPseudo(?string $pseudo): self
     {
         $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
