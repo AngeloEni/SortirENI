954fbfaee989bf57232c319050328db2693d3681
Annest WHELDON <annest.wheldon2021@campus-eni.fr>
2022-03-14 14:38:00 +0100

    creation du API controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Event;
 use App\Entity\Participant;
 use App\Entity\Status;
+use App\Entity\Venue;
 use App\Form\AddEventType;
 use App\Form\SearchEventType;
 use App\Repository\EventRepository;
@@ -77,6 +78,7 @@ class HomeController extends AbstractController
         $user = $this->getParticipantUser();
         $statusCreated = new Status();
         $statusOpen = new Status();
+       // $newVenue = new Venue();
 
         $event = new Event(); // je crée une sortie et un lieu
         //$venues = $venueRepo->findAll();
@@ -102,11 +104,14 @@ class HomeController extends AbstractController
 
         $form = $this->createForm(AddEventType::class, $event);
 
-
         $form->handleRequest($req);
+
+
         if ($form->isSubmitted() && $form->isValid()) {
+
             $isClickedPublish = $form->get('publish')->isClicked();
             $isClickedSave = $form->get('save')->isClicked();
+
 
             if ($isClickedPublish) {
                 $event->setStatus($statusOpen);
